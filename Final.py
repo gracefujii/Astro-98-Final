a484f56a2266e2e6286ebc07695508b622317f12
umireles <umireles@berkeley.edu>
2017-04-14 17:59:11 -0700

    Update Final.py

--- Final.py
+++ Final.py
@@ -1,1 +1,23 @@
+import pygame
 
+
+
+
+
+
+
+
+def game ():  #main game function
+    pygame.init()
+    global(clock,display)  #global variables to be used in other functions
+    clock=pygame.time.clock()
+    display=pygame.display.set_mode(windowwidth,windowheight)  #show the game screen
+    
+    mousex=o
+    mousey=o
+    gameboard=getrandomizedboard()
+    revealedboxes=generaterevealedboxesdata(False)
+    
+    firstchoice=none
+    startgameanimation(gameboard)
+    display.fill
