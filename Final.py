fe25f8e5dac0f5eb148301298b4038634a3d0606
umireles <umireles@berkeley.edu>
2017-04-17 14:57:29 -0700

    Update Final.py

--- Final.py
+++ Final.py
@@ -23,8 +23,36 @@ def game ():  #main game function
         click=false
         display.fill() #fill screen to cover
         drawboard(gameboard,revealedboxes)
-        for event in pg.event.get()
-            if event.type==QUIT
+        for event in pg.event.get():
+            if event.type==QUIT   #if hit quit, quit
                 pg.quit()
                 sys.exit()
-        
+            elif event.type== mousemotion: #otherwise if mouse moves..
+                mousex, mousey= event.pos  #update mouse postion
+            elif event.type== mousebuttonup: # or if mouse was clicked
+                mousex, mousey== mousemotion
+                click=true
+        box_x,box_y=getboxatpixel(mousex, mousey)
+        if box_x==none and box_y==none:   #if mouse over box
+            if not revealedboxes[box_x][box_y]:  #if box is covered
+                drawhighlightbox (box_x,box_y)   #highlight box
+            if not revealedboxes[box_x][box_y] and click:  #if box is covered and clicke
+                revealboxesanimation (gameboard,[(box_x,box_y)])  #reveal box
+                revealedboxes[box_x][box_y]=true  #set box as revealed
+                if firstchoice==none:  #if first box clicked
+                    firstchoice= (box_x,box_y)  #save first choice details
+                else:    #if second box chosen
+                    shape1,color1= getshapeandcolor(gameboard, firstchoice[0], firstchoice[1])  #get shapes ans colors of boxes uncovered
+                    shape2,color2= getshapeandcolor(gameboard, box_x, box_y)
+                    if shape1!= shape2 or color1 != color2
+                        pg.time.wait(1000)
+                        coverboxesanimation(gameboard,[(firstchoice[0],firstchoice[1]),(box_x,box_y)])
+                        revealedboxes[firstchoice[0]][firstchoice[1]]=false
+                        revealedboxes[box_x][box_y]=false
+                    
+                    
+                
+                
+                
+                
+                
