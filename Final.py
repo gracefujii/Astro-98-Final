de49d1f1a730aa400c7c67d5c2083373f267d82d
gracefujii <gracesfujii@gmail.com>
2017-04-18 00:46:23 -0700

    Update Final.py

--- Final.py
+++ Final.py
@@ -1,14 +1,41 @@
 import pygame as pg
 
+FPS = 30
 
+windowwidth = 1000
+windowheight = 600
+boxsize = 60
+gapsize = 10
+boardwidth = 6
+boardheight = 6
+revealspeed = 8
+xmargin = int((windowwidth - (boardwidth * (boxsize + gapsize))) / 2)
+ymargin = int((windowheight - (boardheight * (boxsize + gapsize))) / 2)
 
+BLACK =  (  0,   0,   0)
+WHITE =  (255, 255, 255)
+GRAY =   ( 80, 100, 120)
+BLUE =   (  0, 100, 150)
+GREEN =  ( 20, 150, 100)
+RED =    (200,  50,  60)
+TEAL =   ( 60, 180, 160)
+PURPLE = (100,  80, 120)
+PEACH =  (240, 180, 160)
+YELLOW = (250, 200,  50)
 
+allcolors = (GRAY,GREEN,RED,TEAL,PURPLE,PEACH,YELLOW)
 
+def drawboard(gameboard,revealedboxes):
+    for boxx in range(boardwidth):
+        for boxy in range(boardheight):
+            left,top = lefttopcoords(boxx,boxy)
+            if not revealed[boxx][boxy]:
+                pg.draw.rect(display,BLUE,(left,top,boxsize,boxsize))
 
 def randomizeboard():
     symbols=[]
-    For color in allcolors:
-        For shape in allshapes:
+    for color in allcolors:
+        for shape in allshapes:
             symbols.append((shape,color))  #each item or symbol consist of two attributes (shape, color)
     random.shuffle(symbols) #randomize symbols list
     x=int(boardwitdth*boardlength/2)  #number of symbols used
@@ -29,15 +56,15 @@ def game ():  #main game function
     mousey=o
     gameboard=randomizeboard()  #function to randomize board
     revealedboxes=generaterevealedboxesdata(False)  #all boxes unrevealed
-    display.fill()   #add color background
+    display.fill(WHITE)   #add color background
     firstchoice=none
     startgameanimation(gameboard)  #function to flash symbols underneath boxes
     while true:
         click=false
-        display.fill() #fill screen to cover
+        display.fill(WHITE) #fill screen to cover
         drawboard(gameboard,revealedboxes)
         for event in pg.event.get():
-            if event.type==QUIT   #if hit quit, quit
+            if event.type==QUIT:   #if hit quit, quit
                 pg.quit()
                 sys.exit()
             elif event.type== mousemotion: #otherwise if mouse moves..
@@ -57,7 +84,7 @@ def game ():  #main game function
                 else:    #if second box chosen
                     shape1,color1= getshapeandcolor(gameboard, firstchoice[0], firstchoice[1])  #get shapes ans colors of boxes uncovered
                     shape2,color2= getshapeandcolor(gameboard, box_x, box_y)
-                    if shape1!= shape2 or color1 != color2   #if the symbols do not match...
+                    if shape1!= shape2 or color1 != color2:   #if the symbols do not match...
                         pg.time.wait(1000)   #pause for moment so player can see symbols don't match
                         coverboxesanimation(gameboard,[(firstchoice[0],firstchoice[1]),(box_x,box_y)])  #cover boxes up
                         revealedboxes[firstchoice[0]][firstchoice[1]]=false   #first choice box now unrevealed till next click
